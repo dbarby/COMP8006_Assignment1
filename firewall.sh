fe3c9240b9d9c0ef6e34495a714104ee602298f3
Arnold Myint <blinkmonster@gmail.com>
2018-01-25 16:43:10 -0800

    allow http and https, block port under 1024 for http

--- firewall.sh
+++ firewall.sh
@@ -13,7 +13,27 @@
 
 
 #Permit inbound/outbound ssh packets.
-iptables -A INPUT -p tcp --dport 22
-iptables -A OUTPUT -p tcp --sport 22
+iptables -A INPUT -p tcp --sport 22 ACCEPT
+iptables -A INPUT -p tcp --dport 22 ACCEPT
+
+iptables -A OUTPUT -p tcp --sport 22 ACCEPT
+iptables -A OUTPUT -p tcp --dport 22 ACCEPT
 
 #Permit inbound/outbound www packets.
+
+#http
+iptables -A OUTPUT -p tcp --sport 80 ACCEPT
+iptables -A INPUT -p tcp --dport 80 ACCEPT
+
+iptables -A INPUT -p tcp --sport 80 ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 ACCEPT
+
+#https
+iptables -A OUTPUT -p tcp --sport 443 ACCEPT
+iptables -A INPUT -p tcp --dport 443 ACCEPT
+
+iptables -A INPUT -p tcp --sport 443 ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 ACCEPT
+
+#Drop inbound traffic to port 80 (http) from source ports less than 1024.
+iptables -A INPUT -p tcp --dport 80 --sport 0:1023 -j DROP
